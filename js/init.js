548a8372ded05835f2c3fa00c076617afa0fd926
skychb <gm@haeyum.org>
2017-04-11 09:48:17 +0900

    promotion 1st draft

---
+++ js/init.js
@@ -0,0 +1,8 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('.carousel').carousel();
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
