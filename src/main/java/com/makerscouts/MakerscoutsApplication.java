4c7f6b2f583df668f4b6d7943b1c9327b80051be
skychb <gm@haeyum.org>
2017-04-11 17:50:03 +0900

    first init for spring boot

---
+++ src/main/java/com/makerscouts/MakerscoutsApplication.java
@@ -0,0 +1,12 @@
+package com.makerscouts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakerscoutsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakerscoutsApplication.class, args);
+	}
+}
