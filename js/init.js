ba6663972bfe97f9f2e3e0fbb5aed3f08c8d56b9
skychb <gm@haeyum.org>
2017-04-19 15:51:58 +0900

    change

--- js/init.js
+++ js/init.js
@@ -4,5 +4,8 @@
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
     $('.carousel').carousel();
+    if($('#nav-mobile').css('display')== 'none'){
+      $('.history-caption').removeClass('offset-s1');
+    };
   }); // end of document ready
 })(jQuery); // end of jQuery name space
