a6d3d375edd2b61d9beca2eb644d9a9583064fd5
skychb <gm@haeyum.org>
2017-05-12 17:36:14 +0900

    delete stream_collector

--- src/main/java/com/makerscouts/web/ImageController.java
+++ src/main/java/com/makerscouts/web/ImageController.java
@@ -1,15 +1,11 @@
 package com.makerscouts.web;
 
-import java.io.IOException;
-import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
