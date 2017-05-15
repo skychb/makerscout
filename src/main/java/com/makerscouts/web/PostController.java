f45bf69addf5de470359934a50ddfd9547845b3e
skychb <gm@haeyum.org>
2017-05-15 15:29:28 +0900

    this problem is not solving1!!!!!

--- src/main/java/com/makerscouts/web/PostController.java
+++ src/main/java/com/makerscouts/web/PostController.java
@@ -6,8 +6,6 @@ import java.util.Date;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
