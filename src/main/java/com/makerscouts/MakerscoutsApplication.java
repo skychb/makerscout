7755a900f6b70754de3363d0ebb2d8d20e90626a
skychb <gm@haeyum.org>
2017-05-16 18:39:01 +0900

    confiugure with springbootservletinitializer

--- src/main/java/com/makerscouts/MakerscoutsApplication.java
+++ src/main/java/com/makerscouts/MakerscoutsApplication.java
@@ -3,6 +3,8 @@ package com.makerscouts;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
@@ -11,10 +13,15 @@ import com.makerscouts.configure.Thymeleaf3AutoConfiguration;
 
 @Import(Thymeleaf3AutoConfiguration.class)
 @SpringBootApplication(exclude = ThymeleafAutoConfiguration.class)
-public class MakerscoutsApplication {
+public class MakerscoutsApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(MakerscoutsApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+		return builder.sources(MakerscoutsApplication.class);
 	}
 	
 	@Bean(name = "uploadPath")
