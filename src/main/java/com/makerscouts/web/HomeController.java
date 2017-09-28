6b0ac15b7a8f0a07cc56904a650d0e5fa37c5e91
skychb <gm@haeyum.org>
2017-09-28 15:17:00 +0900

    final before chuseok

--- src/main/java/com/makerscouts/web/HomeController.java
+++ src/main/java/com/makerscouts/web/HomeController.java
@@ -29,7 +29,7 @@ public class HomeController {
 	@Autowired
 	private TeamRepository teamRepository;
 	
-	@RequestMapping(value="/", method=RequestMethod.GET)
+	@RequestMapping(value="/about", method=RequestMethod.GET)
 	public String home() {
 		return "promotion";
 	}
@@ -39,7 +39,7 @@ public class HomeController {
 		return "register";
 	}
 	
-	@GetMapping(value="/home")
+	@GetMapping(value="/")
 	public String realHome(){
 		return "index";
 	}
