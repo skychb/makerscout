dde940de75057f114a195469b0479163653baf6f
skychb <gm@haeyum.org>
2017-09-28 13:41:36 +0900

    debugging

--- src/main/java/com/makerscouts/web/PostController.java
+++ src/main/java/com/makerscouts/web/PostController.java
@@ -6,13 +6,14 @@ import java.util.Date;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.makerscouts.domain.post.News;
 import com.makerscouts.domain.post.NewsRepository;
 import com.makerscouts.domain.post.Post;
 import com.makerscouts.domain.post.PostRepository;
@@ -45,6 +46,29 @@ public class PostController implements Serializable{
 		Post post = postRepository.findOne(id);
 		model.addAttribute("post", post);
 		return "show";
+	}
+	
+	@RequestMapping(value="/edit/{id}")
+	public String getEditingPost(Model model, @PathVariable long id){
+		Post post = postRepository.findOne(id);
+		model.addAttribute("post", post);
+		return "upload";
+	}
+	
+	@RequestMapping(value="edit/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Post> putPost(Post post, @PathVariable long id){
+		Post fetchPost = postRepository.findOne(id);
+		if (fetchPost == null) {
+			return null;
+		}
+		fetchPost.setContents(post.getContents());
+		fetchPost.setTitle(post.getContents());
+		fetchPost.setUpdated(new Date());
+		fetchPost.setAuthor(post.getAuthor());
+		
+		postRepository.save(fetchPost);
+		
+		return new ResponseEntity<Post>(fetchPost, HttpStatus.OK);
 	}
 	
 	@RequestMapping(value="", method=RequestMethod.POST)
